8b0aaff457cf82aca484916b2d90a45a77a76d3a
hugom <hugo.mazeas19@gmail.com>
2024-06-10 22:41:30 -0400

    fight module

--- models/fight.js
+++ models/fight.js
@@ -2,17 +2,39 @@ class Fight {
     static #pool = require('../router/db.js');
     static async get_all_fights() {
         return await this.#pool.query(`
-            SELECT fight.*, 
-            fighter1.first_name AS fighter1_first_name, 
-            fighter1.last_name AS fighter1_last_name, 
-            fighter2.first_name AS fighter2_first_name, 
-            fighter2.last_name AS fighter2_last_name,
-            winner.first_name AS winner_first_name,
-            winner.last_name AS winner_last_name
-        FROM fight 
-        JOIN fighter AS fighter1 ON fight.fighter1_id = fighter1.fighter_id 
-        JOIN fighter AS fighter2 ON fight.fighter2_id = fighter2.fighter_id
-        LEFT JOIN fighter AS winner ON fight.winner_id = winner.fighter_id
+            SELECT 
+            fight.fight_id,
+            fight.event_id,
+            event.name AS event_name,
+            event.date AS event_date,
+            event.location AS event_location,
+            fight.fight_round,
+            fight.number_round,
+            fight.round_length,
+            fight.fight_name,
+            division.division AS division_name,
+            card_type.name AS card_type_name,
+            CONCAT(fighter1.first_name, ' ', fighter1.last_name) AS fighter1_full_name,
+            CONCAT(fighter2.first_name, ' ', fighter2.last_name) AS fighter2_full_name,
+            CASE 
+                WHEN fight.winner_id IS NOT NULL THEN CONCAT(winner.first_name, ' ', winner.last_name)
+                ELSE 'No winner yet'
+            END AS winner_full_name
+        FROM 
+            fight
+        JOIN 
+            event ON fight.event_id = event.event_id
+        JOIN 
+            fighter AS fighter1 ON fight.fighter1_id = fighter1.fighter_id
+        JOIN 
+            fighter AS fighter2 ON fight.fighter2_id = fighter2.fighter_id
+        LEFT JOIN 
+            fighter AS winner ON fight.winner_id = winner.fighter_id
+        JOIN 
+            division ON CAST(fight.division AS INTEGER) = division.division_id
+        JOIN 
+            card_type ON CAST(fight.card_type AS INTEGER) = card_type.card_type_id;
+
             `);
     }
 }
--- router/registry.js
+++ router/registry.js
@@ -6,11 +6,7 @@ const Fight = require('../models/fight');
 
 router.get('/', async function (req, res) {
     const template_suffix = res.locals.template_suffix;
-    const organisation_id = req.params.org_id;
-    const event_id = req.params.event_id;
-    const fight_id = req.params.fight_id;
     const fights = await Fight.get_all_fights();
-    const rounds = (await pool.query('SELECT * FROM public.round WHERE fight_id = $1', [fight_id])).rows;
-    res.render('registry/' + template_suffix + 'Registry', { organisation_id: organisation_id, event_id: event_id, rounds: rounds, fights: fights.rows});
+    res.render('registry/' + template_suffix + 'Registry', { fights: fights.rows });
 });
 module.exports = router;
