773beaa71b97f08a32ccc2dee83a041170b56323
hugom <hugo.mazeas19@gmail.com>
2024-06-16 21:06:10 -0400

    error not picking up winner_id

--- router/API/api_fight.js
+++ router/API/api_fight.js
@@ -8,7 +8,7 @@ router.get('/', async function (req, res) {
 });
 router.post('/', async function (req, res) {
     const fight = req.body;
-    const result = await pool.query('INSERT INTO fight (event_id, fighter1_id, fighter2_id, division, round_length, number_round, card_type, ufc_number) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING fight_id', [fight.event_id, fight.fighter1_id, fight.fighter2_id, fight.division, fight.round_length, fight.number_rounds, fight.card_type, fight.ufc_number]);
+    const result = await pool.query('INSERT INTO fight (event_id, fighter1_id, fighter2_id, division, round_length, number_round, card_type, ufc_number, winner_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING fight_id', [fight.event_id, fight.fighter1_id, fight.fighter2_id, fight.division, fight.round_length, fight.number_rounds, fight.card_type, fight.ufc_number, fight.winner_id]);
     fight.fight_id = result.rows[0].fight_id;
     for (let i = 0; i < fight.number_round; i++) {
         await pool.query('INSERT INTO round (fight_id, round_number, max_duration) VALUES ($1, $2, $3)', [fight.fight_id, i + 1, fight.round_length]);
